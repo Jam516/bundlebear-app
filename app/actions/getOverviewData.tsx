85bd78a230d4d26d5a378b3362b1310564f6411d
Jam516 <kufuorkofi@gmail.com>
2023-09-18 18:07:18 +0100

    commit

--- app/actions/getOverviewData.tsx
+++ app/actions/getOverviewData.tsx
@@ -15,7 +15,7 @@ interface ChainData {
 }
 
 export async function getOverviewData({ chain, timeframe }: OverviewDataParams): Promise<ChainData> {
-    const response = await fetch(`https://bundlebear-api.onrender.com/overview?chain=${chain}&timeframe=${timeframe}`);
+    const response = await fetch(`https://bundlebear-api.onrender.com/overview?chain=${chain}&timeframe=${timeframe}`, { next: { revalidate: 30 } });
 
     if (!response.ok) {
         throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
