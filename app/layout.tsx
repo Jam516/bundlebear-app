da51fdb749c55746ddc54d397833ceeff6cf582a
Jam516 <kufuorkofi@gmail.com>
2023-10-04 23:40:54 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/global.css";
+import { Metadata } from "next"
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { SiteFooter } from "@/components/site-footer";
@@ -6,6 +7,51 @@ import { SiteHeader } from "@/components/site-header";
 import { ChainTabs } from "@/components/chain-tabs";
 import { TimeSelect } from "@/components/time-select";
 import { MobileBlocker } from "@/components/mobile-blocker";
+
+export const metadata: Metadata = {
+  title: {
+    default: "BundleBear",
+    template: `%s - BundleBear`,
+  },
+  description: "ERC4337 Data Analytics",
+  keywords: [
+    "ERC4337",
+    "smart accounts",
+    "account abstraction",
+    "smart contract wallets",
+    "crypto wallet",
+  ],
+  authors: [
+    {
+      name: "0xKofi",
+      url: "https://0xkofi.com",
+    },
+  ],
+  creator: "0xKofi",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.bundlebear.com",
+    title: "BundleBear",
+    description: "ERC4337 Data Analytics",
+    siteName: "BundleBear",
+    images: [
+      {
+        url: "https://i.imgur.com/v4tp3ws.png",
+        width: 1200,
+        height: 630,
+        alt: "BundleBear",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BundleBear",
+    description: "Open and transparent ERC4337 Data",
+    images: ["https://i.imgur.com/v4tp3ws.png"],
+    creator: "@0xKofi",
+  },
+}
 
 interface RootLayoutProps {
   children: React.ReactNode;
